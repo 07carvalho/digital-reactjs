ccf4a1ba998f2085228e5a1307b9eaba9c7c61a2
Felipe Carvalho <felipe.carvalho07@gmail.com>
2020-04-21 10:44:55 -0300

    a small component

--- intro/src/App.js
+++ intro/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
+import Button from './Button';
 import './App.css';
+
+function soma(a, b) {
+	alert(a + b);
+}
 
 function App() {
   return (
     <div className="App">
-    	Hello World!
+    	<h3>Hello World!</h3>
+    	<Button onClick={() => soma(10,20)} name="Soma"/>
     </div>
   );
 }
